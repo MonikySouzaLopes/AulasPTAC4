411899049dd1a71d161b9c8145b93d07c3672928
Moniky <monikysouzalopes@gmail.com>
2024-12-11 21:22:05 -0400

    Atualizado para o código correto dia 11.12

--- src/app/reserva/page.tsx
+++ src/app/reserva/page.tsx
@@ -1,107 +1,130 @@
-"use client";
-
-import { useState } from 'react';
-import styles from './reserva.module.css';
+"use client"
+import Image from "next/image";
+import { stringify } from "querystring";
+import { ChangeEvent, useState } from "react";
 import NavBar from '../componentes/navbar';
-
-export default function Reserva() {
+export default function Home() {
+  function getDateNow (){
+    const today = new Date()
+    return today.toISOString().split("T")[0]
+  }
   const [selectedTable, setSelectedTable] = useState(null);
-  const [formData, setFormData] = useState({
-    usuario: '',
-    mesa: '',
-    numeroPessoas: '',
-    dataReserva: '',
-  });
-
-  const tables = ["Mesa 1", "Mesa 2"];
-
-  const selecionarMesa = (mesaSelecionada) => {
-    setSelectedTable(mesaSelecionada);
-    setFormData((prevState) => ({
-      ...prevState,
-      mesa: mesaSelecionada,
-    }));
-  };
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert(`Reserva realizada com sucesso para a ${formData.mesa}`);
-    setSelectedTable(null);
-    setFormData({
-      usuario: '',
-      mesa: '',
-      numeroPessoas: '',
-      dataReserva: '',
-    });
-  };
-
+  const [dateTables, setDateTables] = useState(getDateNow)
+  const tables = [{id: 1, nome: "Mesa 1"}, {id: 2, nome: "Mesa 2"}, {id: 3, nome: "Mesa 3"}]
+  const reservas = [{
+    id : 1,
+    mesa: 1,
+    data: '2024-11-29'
+  }, 
+  {
+    id : 1,
+    mesa: 2,
+    data: '2024-11-29'
+  },
+  {
+    id : 1,
+    mesa: 2,
+    data: '2024-11-28'
+  }]
+  function handleChangeDate (e: ChangeEvent<HTMLInputElement>) {
+    setDateTables(e.target.value)
+  }
   return (
     <div>
-        <NavBar/>
-    <div className={styles.container}>
-      <h1 className={styles.h1}>Página de Reserva</h1>
-
-      <div className={styles.mesas}>
-        {tables.map((mesa, index) => (
-          <button
-            key={index}
-            className={styles.mesa}
-            onClick={() => selecionarMesa(mesa)}
-          >
-            {mesa}
-          </button>
-        ))}
+      <NavBar/>
+    <div className="min-h-screen bg-gray-100 flex flex-col lg:flex-row">
+      
+      <div className="w-full lg:w-1/4 text-white p-4 flex items-center">
+        <div className="bg-white text-gray-800 rounded-lg shadow-lg p-4 w-full max-w-sm">
+          <img
+            src="/usuario.jpeg"
+            alt="Usuário"
+            className="w-24 h-24 mx-auto rounded-full border-4 border-indigo-500"
+          />
+          <h2 className="text-center text-lg font-bold mt-4">Moniky Souza Lopes</h2>
+          <p className="text-center text-gray-600">Cliente</p>
+        </div>
       </div>
-
-      {selectedTable && (
-        <form className={styles.form} onSubmit={handleSubmit}>
-          <h2 className={styles.titulo}>Reservar {selectedTable}</h2>
-
-          <label className={styles.label}>Usuário:</label>
-          <input
-            className={styles.input}
-            type="text"
-            name="usuario"
-            placeholder="Digite seu nome"
-            value={formData.usuario}
-            onChange={handleChange}
-            required
-          />
-
-          <label className={styles.label}>Número de Pessoas:</label>
-          <input
-            className={styles.input}
-            type="number"
-            name="numeroPessoas"
-            placeholder="Quantas pessoas?"
-            value={formData.numeroPessoas}
-            onChange={handleChange}
-            required
-          />
-
-          <label className={styles.label}>Data de Reserva:</label>
-          <input
-            className={styles.input}
-            type="date"
-            name="dataReserva"
-            value={formData.dataReserva}
-            onChange={handleChange}
-            required
-          />
-
-          <button className={styles.button} type="submit">
-            Confirmar Reserva
-          </button>
-        </form>
-      )}
+      <div className="w-full lg:w-1/2 bg-white p-6">
+        <div>
+          <h2 className="text-xl font-bold mb-4">Mesas Disponíveis</h2>
+          <label className="flex flex-col">
+                <input
+                  type="date"
+                  value={dateTables}
+                  min={dateTables}
+                  className="p-2 border rounded"
+                  onChange={handleChangeDate}
+                />
+          </label>
+        </div>
+        
+        <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 mt-2">
+          {tables.map((table) => {
+          if (reservas.find(reserva => dateTables === reserva.data && reserva.mesa === table.id)){
+            return (
+              <button
+                key={table.id}
+                className="p-4 text-white bg-red-500 rounded-lg hover:bg-red-600 focus:outline-none focus:bg-red-700"
+                onClick={() => setSelectedTable(table.nome)}
+              >
+                {table.nome}
+              </button>
+            )
+          } else {
+          return (
+            <button
+              key={table.id}
+              className="p-4 text-white bg-indigo-500 rounded-lg hover:bg-indigo-600 focus:outline-none focus:bg-indigo-700"
+              onClick={() => setSelectedTable(table.nome)}
+            >
+              {table.nome}
+            </button>
+          )}})}
+        </div>
+      </div>
+      <div className="w-full lg:w-1/4 bg-gray-100 p-4 border-t lg:border-t-0 lg:border-l">
+        {selectedTable ? (
+          <div>
+            <h2 className="text-xl font-bold mb-4">Reservar {selectedTable}</h2>
+            <form className="flex flex-col space-y-4">
+              <label className="flex flex-col">
+                Nome:
+                <input
+                  type="text"
+                  className="p-2 border rounded"
+                  placeholder="Seu nome"
+                />
+              </label>
+              <label className="flex flex-col">
+                Data:
+                <input
+                  type="date"
+                  className="p-2 border rounded"
+                />
+              </label>
+              <label className="flex flex-col">
+                Pessoas:
+                <input
+                  type="number"
+                  max={4}
+                  min={1}
+                
+                  className="p-2 border rounded"
+                />
+              </label>
+              <button
+                type="submit"
+                className="bg-indigo-500 text-white p-2 rounded-lg hover:bg-indigo-600 focus:outline-none focus:bg-indigo-700"
+              >
+                Confirmar Reserva
+              </button>
+            </form>
+          </div>
+        ) : (
+          <p className="text-gray-700">Selecione uma mesa para reservar</p>
+        )}
+      </div>
     </div>
     </div>
   );
